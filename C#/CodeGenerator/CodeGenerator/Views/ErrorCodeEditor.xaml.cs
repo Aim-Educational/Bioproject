42d4ababb00e581709e72e5f8fdc7ef9f29f7292
SealabJaster <sealabjaster@gmail.com>
2018-01-13 07:09:51 +0000

    Design the ErrorCodeEditor

---
+++ C#/CodeGenerator/CodeGenerator/Views/ErrorCodeEditor.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeGenerator.Views
+{
+    /// <summary>
+    /// Interaction logic for ErrorCodeEditor.xaml
+    /// </summary>
+    public partial class ErrorCodeEditor : UserControl
+    {
+        public ErrorCodeEditor(MainWindow window)
+        {
+            InitializeComponent();
+        }
+
+        // Makes it so only numbers can be entered into the textbox.
+        private void textboxCode_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            // false = don't allow text
+            e.Handled = !e.Text.All(c => char.IsDigit(c));
+        }
+    }
+}
